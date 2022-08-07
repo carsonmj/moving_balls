c7d41b4389f6154fe201ec830a69d044dea96dc4
eunoia2040 <eunoia2040@gmail.com>
2022-08-08 02:26:13 +0900

    feat: add ball class

---
+++ src/ball.ts
@@ -0,0 +1,68 @@
+import { Position } from "./position";
+import { v4 as uuidv4 } from "uuid";
+
+interface BallProps {
+  radius: number;
+  color: string;
+  position: IPosition;
+  velocity: Coordinate;
+}
+
+export class Ball {
+  id!: number;
+  type!: string;
+  position!: IPosition;
+  velocity!: Coordinate;
+  radius!: number;
+  color!: string;
+  collisions!: string[];
+
+  constructor(config?: BallProps) {
+    Object.assign(
+      this,
+      {
+        id: uuidv4(),
+        type: "circle",
+        collisions: [],
+      },
+      config
+    );
+  }
+
+  update(state: IStore, updateId: string) {
+    const upperLimit = new Position(state.canvas.canvas.width - this.radius, state.canvas.canvas.height - this.radius);
+    const lowerLimit = new Position(0 + this.radius, 0 + this.radius);
+
+    if (this.position.x >= upperLimit.x || this.position.x <= lowerLimit.x) {
+      this.velocity = new Position(-this.velocity.x, this.velocity.y);
+    }
+
+    if (this.position.y >= upperLimit.y || this.position.y <= lowerLimit.y) {
+      this.velocity = new Position(this.velocity.x, -this.velocity.y);
+    }
+
+    for (let actor of state.actors) {
+      if (this === actor || this.collisions.includes(actor.id + updateId)) {
+        continue;
+      }
+
+      const distance = this.position.subtract(actor.position).magnitude;
+
+      if (distance <= this.radius + actor.radius) {
+        this.velocity = new Position(this.velocity.x * -1, this.velocity.y * -1);
+        actor.velocity = new Position(actor.velocity.x * -1, actor.velocity.y * -1);
+
+        this.collisions.push(actor.id + updateId);
+        actor.collisions.push(this.id + updateId);
+      }
+    }
+
+    const newX = Math.max(Math.min(this.position.x + this.velocity.x, upperLimit.x), lowerLimit.x);
+    const newY = Math.max(Math.min(this.position.y + this.velocity.y, upperLimit.y), lowerLimit.y);
+
+    return new Ball({
+      ...this,
+      position: new Position(newX, newY),
+    });
+  }
+}
