b00d20620cf54187496771c3907e38c115641ca3
eunoia2040 <eunoia2040@gmail.com>
2022-08-08 02:28:17 +0900

    feat: add global type

---
+++ src/global.d.ts
@@ -0,0 +1,36 @@
+interface Coordinate {
+  x: number;
+  y: number;
+}
+
+interface IPosition extends Coordinate {
+  add: (position: Coordinate) => IPosition;
+  subtract: (position: Coordinate) => IPosition;
+  magnitude: number;
+}
+
+interface IBall {
+  id: number;
+  type: string;
+  position: PositionProps;
+  velocity: Coordinate;
+  radius: number;
+  color: string;
+  collisions: string[];
+  update: (state, undateId: string) => IBall;
+}
+
+interface ICanvas {
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D | null;
+  drawCircle: ({ position: Coordinate, radius: number, color: string }) => void;
+  drawActors: (actors: IBall[]) => void;
+  clearDisplay: () => void;
+  render: (state: IStore) => void;
+}
+
+interface IStore {
+  canvas: ICanvas;
+  actors: IBall[];
+  update: () => IStore;
+}
