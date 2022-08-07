a5d7c3e2efb538f136e399c630014821546d80f1
eunoia2040 <eunoia2040@gmail.com>
2022-08-08 03:20:56 +0900

    refactor: update types, code style

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,39 +1,28 @@
-interface CanvasAttributes {
+interface CanvasProps {
   parent: HTMLElement;
   width: number;
   height: number;
 }
 
-interface PositionProps {
-  x: number;
-  y: number;
-}
-
 interface DrawDetails {
-  position: PositionProps;
+  position: Coordinate;
   radius: number;
   color: string;
-}
-
-interface IStore {
-  canvas: any;
-  actors: any[];
-  update: (time: number) => IStore;
 }
 
 export class Canvas {
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D | null;
 
-  constructor(attribyes: CanvasAttributes) {
+  constructor(props: CanvasProps) {
     this.canvas = document.createElement("canvas");
-    this.canvas.width = attribyes.width;
-    this.canvas.height = attribyes.height;
-    attribyes.parent.appendChild(this.canvas);
-    this.ctx = this.canvas.getContext('2d');
+    this.canvas.width = props.width;
+    this.canvas.height = props.height;
+    props.parent.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext("2d");
   }
 
-  drawCircle({position, radius, color}: DrawDetails) {
+  drawCircle({ position, radius, color }: DrawDetails) {
     if (this.ctx) {
       this.ctx.beginPath();
       this.ctx.arc(position.x, position.y, radius, 0, Math.PI * 2);
@@ -43,23 +32,23 @@ export class Canvas {
     }
   }
 
-  sync(state: IStore) {
-    this.clearDisplay();
-    this.drawActors(state.actors);
-  }
-
-  drawActors(actors: any[]) {
+  drawActors(actors: IBall[]) {
     for (let actor of actors) {
-      if (actor.type === 'circle') {
+      if (actor.type === "circle") {
         this.drawCircle(actor);
       }
     }
   }
 
-  clearDisplay () {
+  clearDisplay() {
     if (this.ctx) {
-      this.ctx.fillStyle = '#ffffff';
+      this.ctx.fillStyle = "#ffffff";
       this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     }
+  }
+
+  render(state: IStore) {
+    this.clearDisplay();
+    this.drawActors(state.actors);
   }
 }
