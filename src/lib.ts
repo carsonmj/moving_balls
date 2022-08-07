70c3ac6813ee24d3e38d5e3af070fe2213963e5b
eunoia2040 <eunoia2040@gmail.com>
2022-08-08 03:21:30 +0900

    feat: add helper functions

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,19 @@
+interface XVelocityProps {
+  position: Coordinate;
+  maxRadius: number;
+  min: number;
+  max: number;
+  canvasSize: ICanvasSize;
+}
+
+interface YVelocityProps {
+  min: number;
+  max: number;
+  x: number;
+  maxRadius: number;
+  canvasSize: ICanvasSize;
+}
+
 export const getElement = (selector: string): HTMLInputElement => {
   if (!selector) {
     throw new Error("Parameter is not valid in getElement function. Should pass selector.");
@@ -12,6 +28,17 @@ export const getElement = (selector: string): HTMLInputElement => {
   return element;
 };
 
+export const getRandomNumber = ([low, high]: number[]): number => {
+  return Math.floor(Math.random() * (high - low + 1)) + low;
+};
+
+export const getRandomPosition = (maxRadius: number, canvasSize: ICanvasSize): Coordinate => {
+  return {
+    x: getRandomNumber([maxRadius, canvasSize.width - maxRadius]),
+    y: getRandomNumber([maxRadius, canvasSize.height - maxRadius]),
+  };
+};
+
 export const setEventListener = (event: string, func: () => any): void => {
   if (!event) {
     throw new Error("First parameter is not valid in setEventListener function. Should pass event name.");
@@ -24,6 +51,60 @@ export const setEventListener = (event: string, func: () => any): void => {
   document.addEventListener(event, func);
 };
 
-export const getRandomNumber = ([low, high]: number[]): number => {
-  return Math.floor(Math.random() * (high - low) + low);
+const isValidXPosition = (sign: number, position: number, distance: number, radius: number, max: number): boolean => {
+  let sum = 0;
+
+  if (sign === -1) {
+    sum = position + distance * sign + radius * sign;
+    return sum > 0;
+  }
+
+  sum = position + distance + radius;
+  return sum < max;
+};
+
+const getXVelocity = (props: XVelocityProps): number => {
+  const { position, maxRadius, min, max, canvasSize } = props;
+  const sign = [-1, 1][getRandomNumber([0, 1])];
+  let x = getRandomNumber([Math.sqrt(min), Math.sqrt(max)]);
+
+  while (!isValidXPosition(sign, position.x, x, maxRadius, canvasSize.width)) {
+    x = getRandomNumber([Math.sqrt(min), Math.sqrt(max)]);
+  }
+
+  return x * sign;
+};
+
+const getYVelocity = (props: YVelocityProps): number => {
+  const { min, max, x, maxRadius, canvasSize } = props;
+  const sign = [-1, 1][getRandomNumber([0, 1])];
+  const range = {
+    min: min - x ** 2,
+    max: max - x ** 2,
+  };
+  let y = getRandomNumber([range.min, range.max]) * sign;
+
+  if (y > 0) {
+    const radiusAddedY = y + maxRadius;
+    y = radiusAddedY >= canvasSize.height ? canvasSize.height - maxRadius : y;
+  } else {
+    const radiusAddedY = y + maxRadius;
+    y = radiusAddedY < 0 ? maxRadius : y;
+  }
+
+  return getRandomNumber([range.min, range.max]);
+};
+
+export const getRandomVelocity = (
+  position: Coordinate,
+  perPixel: number[],
+  maxRadius: number,
+  canvasSize: ICanvasSize
+): Coordinate => {
+  const min = Number((perPixel[0] / 60).toFixed(1));
+  const max = Number((perPixel[1] / 60).toFixed(1));
+  const x = getXVelocity({ position, maxRadius, min, max, canvasSize });
+  const y = getYVelocity({ maxRadius, x, min, max, canvasSize });
+
+  return { x, y };
 };
